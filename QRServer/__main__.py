23dd286176957f0dfac936bbacb7f4e1b14e0764
fruktus <fruktusek@gmail.com>
2021-01-23 21:14:21 +0100

    minor fix

--- QRServer/__main__.py
+++ QRServer/__main__.py
@@ -2,13 +2,13 @@ from multiprocessing import Process
 
 from QRServer.lobby.lobby import lobby_listener
 from QRServer.game.game import game_listener
-from config import Config
+from QRServer.config import Config
 from QRServer import lg
 
 
 if __name__ == '__main__':
     lg.info('Quadradius server starting')
-    # TODO handle sigint
+
     lobby_process = Process(target=lobby_listener, args=(Config.HOST, Config.LOBBY_PORT,))
     lobby_process.start()
 

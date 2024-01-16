9dfacba43ff1a305537b42c387f727ae3a59ae70
Kamil Jarosz <kjarosh256@gmail.com>
2024-01-16 18:47:55 +0100

    Use nginx for HTTP & WS proxy

--- docker/qr/http/config-ruffle.js
+++ docker/qr/http/config-ruffle.js
@@ -1,3 +1,6 @@
+let host = window.location.host;
+let protocol = window.location.protocol == 'http:' ? 'ws:' : 'wss:';
+
 window.RufflePlayer = window.RufflePlayer || {};
 window.RufflePlayer.config = {
     autoplay: "on",
@@ -6,12 +9,12 @@ window.RufflePlayer.config = {
         {
             host: "127.0.0.1",
             port: 3000,
-            proxyUrl: window.Quadradius.lobbyProxyUrl,
+            proxyUrl: `${protocol}//${host}/websocket/lobby`,
         },
         {
             host: "127.0.0.1",
             port: 3001,
-            proxyUrl: window.Quadradius.gameProxyUrl,
+            proxyUrl: `${protocol}//${host}/websocket/game`,
         },
     ],
 };

3224a2fea3888af9f75fa6b32648dade6a117c3b
Fruktus <fruktusek@gmail.com>
2025-02-07 15:35:04 +0100

    feat: add ranking table

--- server/src/QRServer/db/migrations.py
+++ server/src/QRServer/db/migrations.py
@@ -1,4 +1,9 @@
-async def setup_metadata(c):
+from datetime import datetime as dt
+from QRServer.common import utils
+
+
+async def setup_metadata(dbconnector):
+    c = await dbconnector.conn.cursor()
     await c.execute(
         "create table if not exists metadata ("
         "  name varchar primary key, "
@@ -6,7 +11,8 @@ async def setup_metadata(c):
         ")")
 
 
-async def execute_migrations(c):
+async def execute_migrations(dbconnector):
+    c = await dbconnector.conn.cursor()
     version = await _select_version(c)
     if version is None:
         await c.execute("insert into metadata (name, value) values ('version', '0')")
@@ -54,6 +60,39 @@ async def execute_migrations(c):
             " add column discord_user_id varchar"
         )
         await _set_version(c, 5)
+    if version <= 5:
+        await c.execute(
+            "create table rankings ("
+            "  year integer,"
+            "  month integer,"
+            "  position integer,"
+            "  user_id varchar,"
+            "  wins integer,"
+            "  total_games integer,"
+            "  primary key (year, month, position),"
+            "  foreign key(user_id) references users (id)"
+            ")"
+        )
+        await c.execute(
+            "select started_at"
+            " from matches"
+            " order by started_at asc"
+            " limit 1"
+        )
+        row = await c.fetchone()
+        if row:
+            first_timestamp = row[0]
+            first_dt = dt.fromtimestamp(first_timestamp)
+            dates_range = utils.make_month_dates_range(start_date=first_dt, end_date=dt.now())
+
+            for date in dates_range:
+                start_date, end_date = utils.make_month_dates(date.month, date.year)
+
+                ranking_entries = await dbconnector._generate_ranking(start_date, end_date)
+                if ranking_entries:
+                    await dbconnector._update_ranking(ranking_entries, year=date.year, month=date.month)
+
+        await _set_version(c, 6)
 
 
 async def _select_version(c):

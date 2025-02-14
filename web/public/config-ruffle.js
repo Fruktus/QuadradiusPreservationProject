e81f3340506c88bd9e849a32a94847b25ff1a668
Fruktus <fruktusek@gmail.com>
2025-02-14 17:32:51 +0100

    feat: set up url rewrite for outdated links

--- web/public/config-ruffle.js
+++ web/public/config-ruffle.js
@@ -18,4 +18,19 @@ window.RufflePlayer.config = {
             proxyUrl: `${protocol}//${host}/websocket/game`,
         },
     ],
+    urlRewriteRules: [
+        // [/^https?:\/\/(www\.)?quadradius\.com\/stuff\/quadradius\/tutorial\/tutorial\.html$/, ...], // Directions -> 99 sec starter video
+        [/^https?:\/\/(www\.)?quadradius\.com\/quadradius\/directions\.html#rankings$/, "/directions.html#rankings"], // (Main) -> Ranking
+        [/^https?:\/\/(www\.)?quadradius\.com\/quadradius\/directions\.html#memberships$/, "/directions.html#memberships"], // Members -> Learn More
+        [/^https?:\/\/(www\.)?quadradius\.com\/quadradius\/directions\.html$/, "/directions.html"], // Directions -> Advanced training
+        [/^https?:\/\/(www\.)?quadradius\.com\/quadradius\/paypal\/PayPalNotice(%20| )1(%20| )month\.html$/, "https://discord.gg/cVkV8pah4d"], // Members -> (Paid Plans)
+        [/^https?:\/\/[^/]+\/about_quadradius\.html$/, "https://github.com/Fruktus/QuadradiusPreservationProject"], // About -> Read more about Quadradius
+        // [/^https?:\/\/[^/]+\/about_jimmi_heiserman\.html$/, ...], // About -> Read more about Jimmi
+        // [/^https?:\/\/[^/]+\/about_brad_kayal\.html$/, ...], // About -> Read more about Brad
+        [/^https?:\/\/(www\.)?quadradius\.com\/quadradius\/PressAndReviews.*$/, "https://discord.gg/cVkV8pah4d"], // Press/Reviews
+        [/^https?:\/\/(www\.)?quadradius\.com\/quadboard\/viewforum\.php\?f=9$/, "https://discord.gg/cVkV8pah4d"], // Fan Qreations
+        [/^https?:\/\/(www\.)?quadradius\.com\/quadboard.*$/, "https://discord.gg/cVkV8pah4d"], // Quadboard
+        // [/^aim:goim\?screenname=Quadradius$/, ...], // Contact -> AIM
+        // [/^callto:\/\/quadradius\/?$/, ...], // Contact -> Skype
+    ],
 };

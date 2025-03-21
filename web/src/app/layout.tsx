97fc1576314f8dcc3d05626fd0828dfbdec96c64
eitanavgil <eitanavgil@gmail.com>
2025-03-21 16:24:09 +0200

    [tech] - refactor

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Press_Start_2P, VT323, Silkscreen } from "next/font/google";
 import "./globals.css";
-import Tabs from "../components/tabs";
-import bgImage from "../assets/qr-bg.jpg";
-
+import Tabs from "./components/tabs";
+import bgImage from "./assets/qr-bg.jpg";
 const tabs = [
   { label: "Game Play", href: "/gameplay" },
   { label: "Interface", href: "/interface" },

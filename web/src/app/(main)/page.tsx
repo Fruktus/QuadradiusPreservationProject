d8ff1b623fdd12199ca4989f66ff0d2430fd0612
Fruktus <fruktusek@gmail.com>
2026-08-26 00:45:28 +0200

    chore: rewrite fullscreen toggle in next.js

--- web/src/app/(main)/page.tsx
+++ web/src/app/(main)/page.tsx
@@ -2,33 +2,19 @@
 
 import Footer from "@/components/ui/footer/footer";
 import Ruffle from "@/components/ruffle/ruffle";
-import Script from "next/script";
 import { useUnloadWarning } from '@/hooks/use-unload-warning';
+import FullscreenToggle from '@/components/ui/fullscreen-toggle/fullscreen-toggle';
 
 export default function Home() {
   useUnloadWarning();
 
   return (
     <>
-      <Script src="/fullscreen.js" />
       <Ruffle />
       <div className="directions">
         <a href="/directions.html" target="_blank">How to play &amp; Powerup Cheatsheet</a>
       </div>
-      <div className="fullscreen">
-        <button id="fullscreen-toggle" title="Toggle fullscreen">
-          <img
-            src="fullscreen-open.svg"
-            id="fullscreen-open"
-            alt="Fullscreen open icon"
-          />
-          <img
-            src="fullscreen-close.svg"
-            id="fullscreen-close"
-            alt="Fullscreen close icon"
-          />
-        </button>
-      </div>
+      <FullscreenToggle />
       <div className="game-container">
         <object className="game">
           <embed src="./quadradius_lobby.swf" className="embed" />
